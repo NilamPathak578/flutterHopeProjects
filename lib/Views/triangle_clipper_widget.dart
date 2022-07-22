2280b44b5ab4798132bfb55e350a688573aafdbe
NilamPathak578 <pathaknilm@gmail.com>
2022-07-22 07:23:13 +0545

    ClipPath Example

---
+++ lib/Views/triangle_clipper_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TriangleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.moveTo(size.width / 2, 0.0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0.0, size.height);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
+
+class MyCustomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width * 0.05, size.height - 200, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
--- lib/cliprrect.dart
+++ lib/cliprrect.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterhopeprojects/Views/triangle_clipper_widget.dart';
 
 class ClipRRectExample extends StatelessWidget {
   const ClipRRectExample({Key? key}) : super(key: key);
@@ -15,21 +16,47 @@ class ClipRRectExample extends StatelessWidget {
         padding: const EdgeInsets.all(12.0),
         child: ListView(
           children: [
-            ClipRRect(
-              borderRadius:
-                  const BorderRadius.vertical(top: Radius.circular(16.0)),
+            // ClipRRect(
+            //   borderRadius:
+            //       const BorderRadius.vertical(top: Radius.circular(16.0)),
+            //   child: Image.asset(
+            //     "assets/Images/image.jpg",
+            //   ),
+            // ),
+            // const SizedBox(
+            //   height: 20.0,
+            // ),
+            // ClipOval(
+            //   child: Image.asset(
+            //     "assets/Images/image.jpg",
+            //   ),
+            // )
+            ClipPath(
+              clipper: TriangleClipper(),
               child: Image.asset(
                 "assets/Images/image.jpg",
               ),
             ),
+
             const SizedBox(
-              height: 20.0,
+              height: 6.0,
             ),
-            ClipOval(
-              child: Image.asset(
-                "assets/Images/image.jpg",
-              ),
-            )
+            ClipPath(
+                clipper: MyCustomClipper(),
+                child: Container(
+                  height: 300,
+                  width: double.infinity,
+                  color: Colors.redAccent,
+                  child: const Center(
+                    child: Text(
+                      "ClipPath Example",
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                  ),
+                )),
           ],
         ),
       ),
